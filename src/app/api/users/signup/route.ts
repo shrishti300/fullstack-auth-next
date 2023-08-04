7548bee6634fb7fc804eca81eee0223c104a7329
shrishti gupta <shrishti0301@gmail.com>
2023-08-04 23:53:22 +0530

    ✨ functionality added

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,6 +2,7 @@ import {connect} from "@/dbconfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from 'bcryptjs';
+import { sendEmail } from "@/helpers/mailer";
 
 
 connect();
@@ -26,8 +27,10 @@ export async function POST(request:NextRequest){
         })
 
         const savedUser = await newUser.save()
-        console.log(savedUser);
+        console.log("saved",savedUser);
+// send verification mail 
 
+await sendEmail({email, emailType:"VERIFY", userId:savedUser._id})
         return NextResponse.json({
             message:"User created successfully",
             success:true,
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname
-  const isPublicPath = path === '/login' || path === '/signup' || path === '/verifyemail'
+  const isPublicPath = path === '/login' || path === '/signup' || path === '/verifyemail' 
 const token = request.cookies.get('token')?.value || ''
 
    if(isPublicPath && token) {
@@ -24,6 +24,6 @@ export const config = {
     '/profile',
     '/login',
     '/signup',
-   
+   '/verifyemail'
   ]
 }
